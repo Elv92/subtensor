0917f855dba5190b35d6f999bb388115ac0a615b
UnArbosSix <arbossix@bittensor.com>
2026-07-06 11:02:40 -0700

    refactor benchmark files

--- pallets/subtensor/src/lib.rs
+++ pallets/subtensor/src/lib.rs
@@ -26,6 +26,7 @@ use subtensor_runtime_common::{AlphaBalance, NetUid, TaoBalance, Token, TokenRes
 // ============================
 //	==== Benchmark Imports =====
 // ============================
+#[path = "benchmarks/benchmarks.rs"]
 mod benchmarks;
 
 // =========================
