09e30b2aeebabf95dc9037eb820e39c74996da5a
UnArbosFive <arbosfive@bittensor.com>
2026-07-13 05:25:02 +0200

    fix: preserve generated calls file terminator

--- sdk/python/bittensor/_generated/calls.py
+++ sdk/python/bittensor/_generated/calls.py
@@ -1583,3 +1583,4 @@ class LimitOrders:
         'Set a status for the limit orders pallet  Must be called by root It allows disabling or enabling the pallet true means enabling, false means disabling'
         return Call('LimitOrders', 'set_pallet_status', {'enabled': enabled})
 
+
